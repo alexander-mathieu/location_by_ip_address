f60f2b97d70b881e46890e9ff1a90d91e4cebf15
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-31 16:49:05 -0600

    Add LocationByIpController#validate_ipv4_address

--- app/controllers/api/v1/location_by_ip_controller.rb
+++ app/controllers/api/v1/location_by_ip_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::LocationByIpController < ApplicationController
+  before_action :validate_ipv4_address, only: :index
+
   def index
     facade = LocationByIpFacade.new(params[:ip])
     location_serializer = LocationByIpSerializer.new(facade.location_info)
     render json: location_serializer.format_response
+  end
+
+  private
+
+  def validate_ipv4_address
+    if !params[:ip].match?(/\b(?:\d{1,3}\.){3}\d{1,3}\b/)
+      render json: { error: 'Please provide an address with valid IPv4 formatting.' }, status: 400
+    end
   end
 end

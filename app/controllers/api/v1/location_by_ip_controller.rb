22665e14585996e09aa0843d9f5ad54cf75103c6
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-10-31 16:28:39 -0600

    Complete LocationByIpController#index

--- app/controllers/api/v1/location_by_ip_controller.rb
+++ app/controllers/api/v1/location_by_ip_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::LocationByIpController < ApplicationController
   def index
-
+    facade = LocationByIpFacade.new(params[:ip])
+    location_serializer = LocationByIpSerializer.new(facade.location_info)
+    render json: location_serializer.format_response
   end
 end
